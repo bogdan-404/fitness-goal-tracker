89d5a1eaab2532383f22b253a48f345c2b183523
bogdan-404 <bogdan.zlatovcen@gmail.com>
2024-09-27 18:04:04 +0300

    delte remaining files

--- api-gateway/src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('status')
-export class AppController {
-  @Get()
-  getStatus() {
-    return { status: 'API Gateway is running' };
-  }
-}
--- user-service/user_service/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import JsonResponse
-
-def status(request):
-    return JsonResponse({"status": "User service is running"})
